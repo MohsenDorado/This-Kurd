f4d1f970c703c3dc674c60feaf715f3fd1bfbc45
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-07-15 23:15:44 +0330

    Create user and add to db

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -19,8 +20,15 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressContentEditableWarning>
-        <body className={font.className}>
-          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="this-kurd-theme">{children}</ThemeProvider>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="this-kurd-theme"
+          >
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
