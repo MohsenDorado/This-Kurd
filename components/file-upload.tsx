71d220121075562e43738c732887c485e8d53faa
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-07-17 11:49:49 +0330

    upload image done!

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,13 +1,39 @@
-"use client"
-interface FileUploadProps{
-    onChange:(url?:string)=>void;
-    value:string;
-    endpoint:"messageFile" | "serverImage"
+"use client";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+
+import "@uploadthing/react/styles.css";
+import { X } from "lucide-react";
+import Image from "next/image";
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "messageFile" | "serverImage";
 }
-const FileUpload = ({endpoint,onChange,value}:FileUploadProps) => {
-    return ( 
-        <div>fileUpload</div>
-     );
-}
- 
+const FileUpload = ({ endpoint, onChange, value }: FileUploadProps) => {
+    const fileType=value.split('.').pop();
+    if (value&&fileType!=="pdf") {
+        return(
+            <div className="relative h-20 w-20">
+                <Image fill src={value} alt="Uploaded Image" className="rounded-full" />
+                <button onClick={()=>onChange("")} className="bg-rose-500 text-white p-1 rounded-full top-0 right-0 shadow-sm absolute "><X className="w-4 h-4"/></button>
+
+            </div>
+        )
+        
+    }
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(err) => {
+        console.log(err);
+      }}
+    />
+  );
+};
+
 export default FileUpload;
